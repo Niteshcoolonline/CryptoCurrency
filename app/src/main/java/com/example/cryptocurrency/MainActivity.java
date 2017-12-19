f337657bb4904f2340201a1b72d413a42825acf7
nitesh <mrniteshdiwan@gmail.com>
2017-12-20 03:06:30 +0530

    listview done with all coin prices

--- app/src/main/java/com/example/cryptocurrency/MainActivity.java
+++ app/src/main/java/com/example/cryptocurrency/MainActivity.java
@@ -25,17 +25,20 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import org.json.JSONTokener;
 
+import java.util.Iterator;
+
 public class MainActivity extends AppCompatActivity {
 
     JSONArray arr;
-    String[] name={"BitCoin"};
+    String[] key=new String[60];
+    String[] name=new String[60];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView listview=(TextView) findViewById(R.id.d);
+        ListView listview=(ListView) findViewById(R.id.listing);
         final RequestQueue requestQueue=Volley.newRequestQueue(MainActivity.this);
-        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,"https://www.buyucoin.com/api/v1/btc/",(String) null, new Response.Listener<JSONObject>() {
+        JsonObjectRequest jsonObjectRequest=new JsonObjectRequest(Request.Method.GET,"https://www.buyucoin.com/api/v1/crypto/",(String) null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
 
@@ -43,7 +46,16 @@ public class MainActivity extends AppCompatActivity {
                     arr=response.getJSONArray("BuyUcoin_data");
                     for(int j=0;j<arr.length();j++) {
                         JSONObject prices = arr.getJSONObject(j);
-                        listview.setText(prices.getString("btc_buy_price"));
+                        Iterator<String> it= prices.keys();
+                        int i=0;
+                        while (it.hasNext()){
+                            key[i]=it.next();
+                            name[i]=prices.getString(key[i]);
+                            Log.d(String.valueOf(MainActivity.this),"key:-"+key[i]+"Value"+name[i]);
+                            i++;
+                        }
+
+                        Log.d(String.valueOf(MainActivity.this),String.valueOf(prices));
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -56,33 +68,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         requestQueue.add(jsonObjectRequest);
-//        customAdapter customadapter=new customAdapter();
-//        listview.setAdapter(customadapter);
+        customAdapter customadapter=new customAdapter();
+        listview.setAdapter(customadapter);
     }
-//    class customAdapter extends BaseAdapter {
-//
-//        @Override
-//        public int getCount() {
-//            return name.length;
-//        }
-//
-//        @Override
-//        public Object getItem(int i) {
-//            return null;
-//        }
-//
-//        @Override
-//        public long getItemId(int i) {
-//            return 0;
-//        }
-//
-//        @Override
-//        public View getView(int i, View view, ViewGroup viewGroup) {
-//            view=getLayoutInflater().inflate(R.layout.customlayout,null);
-//            TextView tname=(TextView)view.findViewById(R.id.textname);
-//            TextView tprice=(TextView)view.findViewById(R.id.textprice);
-//
-//            return view;
-//        }
-//    }
+    class customAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return name.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view=getLayoutInflater().inflate(R.layout.customlayout,null);
+            TextView tname=(TextView)view.findViewById(R.id.textname);
+            TextView tprice=(TextView)view.findViewById(R.id.textprice);
+            tname.setText(key[i]);
+            tprice.setText(name[i]);
+            return view;
+        }
+    }
 }
